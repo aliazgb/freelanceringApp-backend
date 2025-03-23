6b6a0b9bb03dc4b33502a17ba94edf3117ed4806
YourGitHubUsername <YourEmail@example.com>
2025-03-23 16:43:55 +0100

    Initial commit

---
+++ app/http/controllers/userAuth.controller.js
@@ -0,0 +1,197 @@
+const Controller = require("./controller");
+const {
+  generateRandomNumber,
+  toPersianDigits,
+  setAccessToken,
+  setRefreshToken,
+  verifyRefreshToken,
+} = require("../../../utils/functions");
+const createError = require("http-errors");
+const { UserModel } = require("../../models/user");
+const CODE_EXPIRES = 90 * 1000; //90 seconds in milliseconds
+const { StatusCodes: HttpStatus } = require("http-status-codes");
+const {
+  completeProfileSchema,
+  updateProfileSchema,
+  checkOtpSchema,
+} = require("../validators/user.schema");
+
+class userAuthController extends Controller {
+  constructor() {
+    super();
+    this.code = 0;
+    this.phoneNumber = null;
+  }
+  async getOtp(req, res) {
+    let { phoneNumber } = req.body;
+
+    if (!phoneNumber)
+      throw createError.BadRequest("شماره موبایل معتبر را وارد کنید");
+
+    phoneNumber = phoneNumber.trim();
+    this.phoneNumber = phoneNumber;
+    this.code = generateRandomNumber(6);
+
+    const result = await this.saveUser(phoneNumber);
+    if (!result) throw createError.Unauthorized("ورود شما انجام نشد.");
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: {
+        message: `کد تایید شما برای شماره ${phoneNumber} ارسال گردید `,
+        // otp: this.code, // اضافه کردن OTP در پاسخ
+        expiresIn: CODE_EXPIRES,
+        phoneNumber,
+      },
+    });
+  }
+  async checkOtp(req, res) {
+    await checkOtpSchema.validateAsync(req.body);
+    const { otp: code, phoneNumber } = req.body;
+
+    const user = await UserModel.findOne(
+      { phoneNumber },
+      { password: 0, refreshToken: 0, accessToken: 0 }
+    );
+
+    if (!user) throw createError.NotFound("کاربری با این مشخصات یافت نشد");
+
+    if (user.otp.code != code)
+      throw createError.BadRequest("کد ارسال شده صحیح نمیباشد");
+
+    if (new Date(`${user.otp.expiresIn}`).getTime() < Date.now())
+      throw createError.BadRequest("کد اعتبار سنجی منقضی شده است");
+
+    user.isVerifiedPhoneNumber = true;
+    await user.save();
+
+    await setAccessToken(res, user);
+    await setRefreshToken(res, user);
+    let WELLCOME_MESSAGE = `کد تایید شد، به فرانت هوکس خوش آمدید`;
+    if (!user.isActive)
+      WELLCOME_MESSAGE = `کد تایید شد، لطفا اطلاعات خود را تکمیل کنید`;
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: {
+        message: WELLCOME_MESSAGE,
+        user,
+      },
+    });
+  }
+  async saveUser(phoneNumber) {
+    const otp = {
+      code: this.code,
+      expiresIn: Date.now() + CODE_EXPIRES,
+    };
+
+    const user = await this.checkUserExist(phoneNumber);
+    if (user) return await this.updateUser(phoneNumber, { otp });
+
+    return await UserModel.create({
+      phoneNumber,
+      otp,
+    });
+  }
+  async checkUserExist(phoneNumber) {
+    const user = await UserModel.findOne({ phoneNumber });
+    return user;
+  }
+  async updateUser(phoneNumber, objectData = {}) {
+    Object.keys(objectData).forEach((key) => {
+      if (["", " ", 0, null, undefined, "0", NaN].includes(objectData[key]))
+        delete objectData[key];
+    });
+    const updatedResult = await UserModel.updateOne(
+      { phoneNumber },
+      { $set: objectData }
+    );
+    return !!updatedResult.modifiedCount;
+  }
+  async completeProfile(req, res) {
+    await completeProfileSchema.validateAsync(req.body);
+    const { user } = req;
+    const { name, email, role } = req.body;
+
+    if (!user.isVerifiedPhoneNumber)
+      throw createError.Forbidden("شماره موبایل خود را تایید کنید.");
+
+    const duplicateUser = await UserModel.findOne({ email });
+    if (duplicateUser)
+      throw createError.BadRequest(
+        "کاربری با این ایمیل قبلا ثبت نام کرده است."
+      );
+
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: user._id },
+      { $set: { name, email, isActive: true, role } },
+      { new: true }
+    );
+    await setAccessToken(res, updatedUser);
+    await setRefreshToken(res, updatedUser);
+
+    return res.status(HttpStatus.OK).send({
+      statusCode: HttpStatus.OK,
+      data: {
+        message: "اطلاعات شما با موفقیت تکمیل شد",
+        user: updatedUser,
+      },
+    });
+  }
+  async updateProfile(req, res) {
+    const { _id: userId } = req.user;
+    await updateProfileSchema.validateAsync(req.body);
+    const { name, email, biography, phoneNumber } = req.body;
+
+    const updateResult = await UserModel.updateOne(
+      { _id: userId },
+      {
+        $set: { name, email, biography, phoneNumber },
+      }
+    );
+    if (!updateResult.modifiedCount === 0)
+      throw createError.BadRequest("اطلاعات ویرایش نشد");
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: {
+        message: "اطلاعات با موفقیت آپدیت شد",
+      },
+    });
+  }
+  async refreshToken(req, res) {
+    const userId = await verifyRefreshToken(req);
+    const user = await UserModel.findById(userId);
+    await setAccessToken(res, user);
+    await setRefreshToken(res, user);
+    return res.status(HttpStatus.OK).json({
+      StatusCode: HttpStatus.OK,
+      data: {
+        user,
+      },
+    });
+  }
+  async getUserProfile(req, res) {
+    const { _id: userId } = req.user;
+    const user = await UserModel.findById(userId, { otp: 0 });
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: {
+        user,
+      },
+    });
+  }
+  logout(req, res) {
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    return res.status(HttpStatus.OK).json({
+      StatusCode: HttpStatus.OK,
+      roles: null,
+      auth: false,
+    });
+  }
+}
+
+module.exports = {
+  UserAuthController: new userAuthController(),
+};
