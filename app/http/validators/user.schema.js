1be67c1fd1da7a010ba2da8ec013aa7e0e5378e2
Ali Azarkashb <ali.azgb10@gmail.com>
2025-03-26 00:33:18 +0100

    Save unstaged changes

--- app/http/validators/user.schema.js
+++ app/http/validators/user.schema.js
@@ -3,32 +3,32 @@ const createHttpError = require("http-errors");
 
 const getOtpSchema = Joi.object({
   phoneNumber: Joi.string()
-    .pattern(/^(0|(\+98|\+49))[1-9][0-9]{9,10}$/)  // ترکیب برای پشتیبانی از 0، +98، +49
-    .error(createHttpError.BadRequest("شماره موبایل وارد شده صحیح نمیباشد")),
+    .pattern(/^(0|(\+98|\+49))[1-9][0-9]{9,10}$/)  
+    .error(createHttpError.BadRequest("The entered phone number is invalid")),
 });
 
 const checkOtpSchema = Joi.object({
   otp: Joi.string()
     .min(5)
     .max(6)
-    .error(createHttpError.BadRequest("کد ارسال شده صحیح نمیباشد")),
+    .error(createHttpError.BadRequest("The entered OTP is invalid")),
   phoneNumber: Joi.string()
-    .pattern(/^(0|(\+98|\+49))[1-9][0-9]{9,10}$/)  // ترکیب برای پشتیبانی از 0، +98، +49
-    .error(createHttpError.BadRequest("شماره موبایل وارد شده صحیح نمیباشد")),
+    .pattern(/^(0|(\+98|\+49))[1-9][0-9]{9,10}$/)  
+    .error(createHttpError.BadRequest("The entered phone number is invalid")),
 });
 
 const completeProfileSchema = Joi.object({
   name: Joi.string()
     .min(5)
     .max(100)
-    .error(createHttpError.BadRequest("نام کاربری وارد شده صحیح نمی باشد")),
+    .error(createHttpError.BadRequest("The entered username is invalid")),
   email: Joi.string()
     .email()
-    .error(createHttpError.BadRequest("ایمیل وارد شده صحیح نمی باشد")),
+    .error(createHttpError.BadRequest("The entered email is invalid")),
   role: Joi.string()
     .required()
     .valid("FREELANCER", "OWNER")
-    .error(createHttpError.BadRequest("نقش وارد شده صحیح نمی باشد")),
+    .error(createHttpError.BadRequest("The entered role is invalid")),
 });
 
 const updateProfileSchema = Joi.object({
@@ -42,7 +42,7 @@ const updateProfileSchema = Joi.object({
     .email()
     .error(createHttpError.BadRequest("Invalid email entered")),
   phoneNumber: Joi.string()
-    .pattern(/^(0|(\+98|\+49))[1-9][0-9]{9,10}$/)  // ترکیب برای پشتیبانی از 0، +98، +49
+    .pattern(/^(0|(\+98|\+49))[1-9][0-9]{9,10}$/)  
     .error(createHttpError.BadRequest("Invalid phone number entered")),
   biography: Joi.string()
     .max(30)
