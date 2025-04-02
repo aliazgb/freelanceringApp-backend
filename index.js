8f54da3cacdff17e0e089cd097da8958fc6588de
Ali Azarkashb <ali.azgb10@gmail.com>
2025-04-02 23:39:07 +0200

    fixed app on safari browser

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const Application = require("./app/server.js");
-new Application();
+app.use(cors({
+    origin: "https://freelancerin-app-project-8dd2.vercel.app/",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }));
+  
