f5b16be5dbea3a81a39f9a1825f37d4d6df4b4ba
Ali Azarkashb <ali.azgb10@gmail.com>
2025-04-02 23:46:54 +0200

    Fix something

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-app.use(cors({
-    origin: "https://freelancerin-app-project-8dd2.vercel.app/",
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  }));
-  
+const Application = require("./app/server.js");
+new Application();
